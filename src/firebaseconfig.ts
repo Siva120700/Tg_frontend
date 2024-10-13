fedf587f0f9ff701b7005561c7867ed12688e397
Sivaraman S <160136739+Siva120700@users.noreply.github.com>
2024-10-13 23:19:04 +0530

    Update firebaseconfig.ts

--- src/firebaseconfig.ts
+++ src/firebaseconfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
+    apiKey: "AIzaSyDzsPAJbBvLnbTIfXjfuQlrvmJayEml_VQ",
     authDomain: "truegradient-e1ac6.firebaseapp.com",
     projectId: "truegradient-e1ac6",
     storageBucket: "truegradient-e1ac6.appspot.com",
