6bbca8fce22d4b21274e686f20905c8b4c94f8bc
Sivaraman S <160136739+Siva120700@users.noreply.github.com>
2024-10-13 22:50:03 +0530

    Update firebaseconfig.ts

--- src/firebaseconfig.ts
+++ src/firebaseconfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzsPAJbBvLnbTIfXjfuQlrvmJayEml_VQ",
-  authDomain: "truegradient-e1ac6.firebaseapp.com",
-  projectId: "truegradient-e1ac6",
-  storageBucket: "truegradient-e1ac6.appspot.com",
-  messagingSenderId: "61665199379",
-  appId: "1:61665199379:web:fb1d582f30562ce76606e6",
-  measurementId: "G-EDJPYHWWWT"
+    apiKey: "AIzaSyDzsPAJbBvLnbTIfXjfuQlrvmJayEml_VQ",
+    authDomain: "truegradient-e1ac6.firebaseapp.com",
+    projectId: "truegradient-e1ac6",
+    storageBucket: "truegradient-e1ac6.appspot.com",
+    messagingSenderId: "61665199379",
+    appId: "1:61665199379:web:fb1d582f30562ce76606e6",
+    measurementId: "G-EDJPYHWWWT"
 };
 
 const app = initializeApp(firebaseConfig);
